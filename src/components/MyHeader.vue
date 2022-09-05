d92f881f24bea0c302b1d369b0192d427154de8b
Marta Iachini <iachinimarta@gmail.com>
2022-09-05 19:39:37 +0200

    Modificihe: - inserito jumbotron - inserita sezione "beige" - iniziata sezione "latest news"

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,31 +1,30 @@
 <template>
   <header>
-    <!-- HEADER -->
-    <div class="container-fluid bg_brandColor">
-      <div class="row ">
-        <div class="col"></div>
-        <div class="col-8">
-          <div class="row">
-            <div class="col-3">
-              <img class="img-fluid" src="../assets/logo-2x.png" alt="logo">
-            </div>
-            <ul class="col-9 d-flex justify-content-end">
-              <li v-for="(link, index) in headerLinks" :key="index"> <a :href="link.link">{{link.name}}</a> </li>
-            </ul>
-          </div>
-        </div>
-        <div class="col"></div>
-      </div>
-    </div>
 
-    <!-- JUMBOTRON -->
-    <div class="container-fluid jumbotron">
-      <div class="row ">
-        <div class="col"></div>
-        <div class="col-8">AREA TESTO</div>
-        <div class="col"></div>
-      </div>
+    <div class="bg_brandColor">
+      <div class="header_container w_80 d_flex">
+        <div class="header_logo">
+          <a href="#">
+            <img src="../assets/logo-2x.png" alt="logo">
+          </a>
+        </div>
+
+        <div class="header_links d_flex">
+          <ul class="d_flex">
+            <li v-for="(links, index) in headerLinks" :key="index">
+              <a :href="links.link">{{links.name}}</a>
+            </li>
+          </ul>
+
+          <button class="btn btn_1">
+            <a href="#">JOIN US</a>
+          </button>
+
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>    
     </div>
+        
   </header>
 </template>
 
@@ -68,11 +67,29 @@
 <style lang="scss">
   @import '@/styles/general.scss';
 
-  .jumbotron {
-    height: 300px;
+  header {
     width: 100%;
-    background-image: url('../assets/home_slider_bg.jpg');
-    background-position: center;
-  }
+    height: 100px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
 
+    .header_container {
+      color: white;
+      justify-content: space-between;
+      align-items: center;
+      padding: 15px;
+
+      .header_links {
+        align-items: center;
+      }
+
+      .header_logo {
+        img {
+          width: 200px;
+        }
+      }
+    }
+  }
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,5 +1,47 @@
 <template>
   <main>
+
+    <!-- JUMBOTRON -->
+
+    <div class="jumbotron d_flex">
+      <div class="jumbotron_text">
+        <h1>Tech Forum!</h1>
+        <p>Discuss all the latest technology news and trends</p>
+        <button class="btn btn_2">
+          <a href="#">GET AVADA NOW! ></a>
+        </button>
+      </div>
+    </div>
+
+    <!-- SEZIONE PLACE TO BE -->
+    <div class="place_to_be t_center">
+      <div class="place_to_be_text w_80">
+        <h5>AVADA FORUM IS THE PLACE TO BE</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit laboriosam debitis hic. Cumque facere sed veritatis ea inventore, et minima eum neque atque repellendus asperiores voluptatibus corrupti aut dolor quisquam sint, nihil deleniti delectus maiores magnam qui perferendis voluptate quaerat eos. Aspernatur ipsam rerum possimus eligendi tempore ducimus rem explicabo.</p>  
+      </div>
+    </div>
+
+    <!-- SEZIONE ALL THE LATEST NEWS -->
+
+    <div>
+      <div class="t_center w_80">
+        <h5>PHASELLUS EGET METUS</h5>
+        <h2>All the latest news</h2>
+      </div>
+    </div>
+
+    <hr>
+
+    <!-- SEZIONE 3 IMGS -->
+
+    <div>
+      <div class="card">
+
+        <!-- creare un array di img da stampare -->
+        <img src="" alt="">
+      </div>
+    </div>
+
   </main>
 </template>
 
@@ -9,6 +51,37 @@
     }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/styles/general.scss';
+
+  main {
+    margin-top: 80px;
+    
+    .jumbotron {
+      color: white;
+      background-image: url('../assets/home_slider_bg.jpg');
+      background-size: cover;
+      background-position: center;
+      min-height: 550px;
+      justify-content: center;
+      align-items: center;
+
+      .jumbotron_text {
+        h1 {
+          font-size: 6rem;
+        }
+      }
+    }
+
+    .place_to_be {
+      background-color: $brand-color-beige;
+    }
+
+    .card {
+      width: calc(100% / 3);
+    }
+  }
+
+  
 
 </style>
