d5369c4da1ded016bb7e6a42a0ca647d5712abf3
Marta Iachini <iachinimarta@gmail.com>
2022-09-05 12:55:42 +0200

    Modifiche: - Aggiunto logo in header - creato array per i link dell'header - prove con il jumbotron

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,14 +1,78 @@
 <template>
   <header>
+    <!-- HEADER -->
+    <div class="container-fluid bg_brandColor">
+      <div class="row ">
+        <div class="col"></div>
+        <div class="col-8">
+          <div class="row">
+            <div class="col-3">
+              <img class="img-fluid" src="../assets/logo-2x.png" alt="logo">
+            </div>
+            <ul class="col-9 d-flex justify-content-end">
+              <li v-for="(link, index) in headerLinks" :key="index"> <a :href="link.link">{{link.name}}</a> </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col"></div>
+      </div>
+    </div>
+
+    <!-- JUMBOTRON -->
+    <div class="container-fluid jumbotron">
+      <div class="row ">
+        <div class="col"></div>
+        <div class="col-8">AREA TESTO</div>
+        <div class="col"></div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
     export default {
-        name: 'MyHeader'
+        name: 'MyHeader',
+        data() {
+          return {
+            headerLinks: [
+              {
+                name: 'Home',
+                link: '#'
+              },
+              {
+                name: 'Apple',
+                link: '#'
+              },
+              {
+                name: 'Microsoft',
+                link: '#'
+              },
+              {
+                name: 'Android',
+                link: '#'
+              },
+              {
+                name: 'Forums',
+                link: '#'
+              },
+              {
+                name: 'Contact us',
+                link: '#'
+              },
+            ]
+          }
+        }
     }
 </script>
 
-<style>
+<style lang="scss">
+  @import '@/styles/general.scss';
+
+  .jumbotron {
+    height: 300px;
+    width: 100%;
+    background-image: url('../assets/home_slider_bg.jpg');
+    background-position: center;
+  }
 
 </style>
