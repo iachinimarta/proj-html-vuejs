b3dfec8b56ca0248def483cf69af35399215cd90
Marta Iachini <iachinimarta@gmail.com>
2022-09-06 12:43:34 +0200

    Modifiche: - impostata sezione "all the latest news" - mancante sezione "tutorial & guides"

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -8,13 +8,13 @@
         <h1>Tech Forum!</h1>
         <p>Discuss all the latest technology news and trends</p>
         <button class="btn btn_2">
-          <a href="#">GET AVADA NOW! ></a>
+          <a href="#">GET AVADA NOW! <i class="fa-solid fa-angle-right"></i></a>
         </button>
       </div>
     </div>
 
     <!-- SEZIONE PLACE TO BE -->
-    <div class="place_to_be t_center">
+    <div class="bg_beige t_center">
       <div class="place_to_be_text w_80">
         <h5>AVADA FORUM IS THE PLACE TO BE</h5>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit laboriosam debitis hic. Cumque facere sed veritatis ea inventore, et minima eum neque atque repellendus asperiores voluptatibus corrupti aut dolor quisquam sint, nihil deleniti delectus maiores magnam qui perferendis voluptate quaerat eos. Aspernatur ipsam rerum possimus eligendi tempore ducimus rem explicabo.</p>  
@@ -31,24 +31,111 @@
     </div>
 
     <hr>
+  
+      <div class="w_80 ">
 
-    <!-- SEZIONE 3 IMGS -->
+        <!-- 3 IMGS -->
+        <MyNewsItems :shareData="newsData_1"/>
 
-    <div>
-      <div class="card">
+        <!-- SEZIONE FEATURED ARTICLES -->
+        <div class="article featured_article">
+          <h5>FEATURED ARTICLE</h5>
+          <h1>Cras malesuada et orci eget pharetra</h1>
+          <p>In quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum sadipis ets ipsums dolores ficilis uns leo lectus.</p>
+          <button class="btn btn_1">
+            <a href="#">READ MORE <i class="fa-solid fa-angle-right"></i></a>
+          </button>
+        </div>
 
-        <!-- creare un array di img da stampare -->
-        <img src="" alt="">
+        <!-- 3 IMGS -->
+        <MyNewsItems :shareData="newsData_2"/>
+
+        <!-- SEZIONE REVIEWED PRODUCT -->
+        <div class="article reviewd_product">
+          <h5>FEATURED ARTICLE</h5>
+          <h1>Cras malesuada et orci eget pharetra</h1>
+          <p>In quis lectus sed leo elementum faucibus in dapibus dictum. Nullamolestie tortor nec lectus venenatis, sed blandit dui, dolor at bibendum sadipis ets ipsums dolores ficilis uns leo lectus.</p>
+          <button class="btn btn_1">
+            <a href="#">READ MORE <i class="fa-solid fa-angle-right"></i></a>
+          </button>
+        </div>
       </div>
-    </div>
 
   </main>
 </template>
 
 <script>
-    export default {
-        name: 'MyMain'
-    }
+import MyNewsItems from '../components/MyNewsItems.vue';
+
+  export default {
+      name: 'MyMain',
+      components: {
+        MyNewsItems,
+      },
+      data() {
+        return {
+          newsData_1: [
+                  {
+                    src: require('../assets/post_feat_img_25-700x441.jpg'),
+                    alt: 'smartphone',
+                    title: 'Morbi vitae dui euismod vulputate sollicitudin',
+                    date: 'October 11th, 2015',
+                    comments: '2 Comments',
+                    active: true,
+                    p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+                  },
+                  {
+                    src: require('../assets/post_feat_img_24-700x441.jpg'),
+                    alt: 'smartphone',
+                    title: 'Vivamus pellenteque, felis quis varius',
+                    date: 'October 11th, 2015',
+                    comments: '2 Comments',
+                    active: false,
+                    p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+                  },
+                  {
+                    src: require('../assets/post_feat_img_23-700x441.jpg'),
+                    alt: 'smartwatch',
+                    title: 'Donec ornare pretium eget scelisque justo',
+                    date: 'October 11th, 2015',
+                    comments: '2 Comments',
+                    active: false,
+                    p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+                  },
+          ],
+          newsData_2: [
+            {
+              src: require('../assets/post_feat_img_22-700x441.jpg'),
+              alt: 'tablet',
+              title: 'Fusce sollicitudin nunc sed placerat varius',
+              date: 'October 11th, 2015',
+              comments: '2 Comments',
+              active: false,
+              p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+            },
+            {
+              src: require('../assets/post_feat_img_21-700x441.jpg'),
+              alt: 'camera',
+              title: 'Donec facilis sodales leo sit amet laoreet',
+              date: 'October 11th, 2015',
+              comments: '2 Comments',
+              active: false,
+              p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+            },
+            {
+              src: require('../assets/post_feat_img_20-700x441.jpg'),
+              alt: 'laptop',
+              title: 'Proin eu purus sed aru aliquet curabir vens',
+              date: 'October 11th, 2015',
+              comments: '2 Comments',
+              active: false,
+              p: 'Donec finibus sit amet orci eget ultricies. Praesent posuere ante ut erat fringilla, vestibulum placerat metus mattis. Aenean dictum vitae nisl'
+            },
+          ],
+        }
+      }
+  }
+
 </script>
 
 <style lang="scss">
@@ -73,13 +160,21 @@
       }
     }
 
-    .place_to_be {
-      background-color: $brand-color-beige;
+    .article {
+      min-height: 400px;
+      background-size: cover;
+      background-position: center;
+      color: white;
     }
 
-    .card {
-      width: calc(100% / 3);
+    .featured_article {
+      background-image: url('../assets/featured_article_1_bg.jpg');
     }
+
+    .reviewd_product {
+      background-image: url('../assets/featured_article_2_bg.jpg');
+    }
+    
   }
 
   
---
+++ src/components/MyNewsItems.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="news_wrapper d_flex" >
+    <div class="news" v-for="(news, index) in shareData" :key="index">
+        <img class="news_img" :src="news.src" :alt="news.alt">
+        <h4>{{news.title}}</h4>
+        <p>{{news.date}} | </p>
+        <p v-if="news.active">{{news.comments}}</p>
+        <p v-else>Comments Off</p>
+        <p>{{news.p}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyNewsItems',
+        data() {
+            return {}                
+        },
+        props: {
+            shareData: Array
+        }
+    }
+</script>
+
+<style lang="scss">
+@import '@/styles/general.scss';
+
+.news_wrapper {
+    width: 100%;
+    height: 100%;
+    flex-wrap: wrap;
+
+    .news {
+        width: calc(100% / 3);
+
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
